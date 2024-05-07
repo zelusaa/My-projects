f5047e77a4f945c8470f5307537b5acae002432c
zelusaa <137693251+zelusaa@users.noreply.github.com>
2024-05-08 00:33:13 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,222 +1,283 @@
-'use strict';
-
-///////////////////////////////////////
-// Modal window
-
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-const section1 = document.querySelector('#section--1');
-const navLinks = document.querySelector('.nav__links');
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+"use strict";
+// Poneze nema dostatacno vreme za vsicko kato aprkosimacija vzeto e da v svak sklad ima tocno mnogo dronove koite tip kmetat moze da odbere pri pridavaneto na sklada v mapata
+const typeOfDrones = [
+  {
+    capacity: 500,
+    consumption: 1,
+  },
+  {
+    capacity: 1000,
+    consumption: 3,
+  },
+  {
+    capacity: 2000,
+    consumption: 5,
+  },
+];
+const output = {
+  poweredOn: true,
+  minutes: {
+    program: 10,
+    real: 400,
+  },
 };
 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-btnsOpenModal.forEach(dugme => {
-  dugme.addEventListener('click', openModal);
+/*---------Add number of products------ */
+const divNumberProducts = document.querySelector(
+  ".select--number--of--products"
+);
+const selectElement = document.getElementById("products");
+selectElement.addEventListener("change", function (e) {
+  e.preventDefault();
+  Array.from(selectElement.selectedOptions).forEach((option) => {
+    const label = document.createElement("label");
+    label.textContent = `Number of ${option.value}: `;
+    const input = document.createElement("input");
+    input.type = "number";
+    input.classList.add("brojProizvodite");
+    input.name = option.value + "-kolicina";
+    divNumberProducts.appendChild(label);
+    divNumberProducts.appendChild(input);
+    divNumberProducts.appendChild(document.createElement("br"));
+  });
 });
-//for (let i = 0; i < btnsOpenModal.length; i++)
-//btnsOpenModal[i].addEventListener('click', openModal);
-
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
+/*--------------------- */
+const formata = document.querySelector(".form--final");
+const displayBtn = document.querySelector(".display--btn");
+const divRezultatite = document.querySelector(".results--div");
+const townFormata = document.querySelector(".town--form");
+const productFormata = document.querySelector(".product--form");
+const custFormata = document.querySelector(".customer--form");
+const skladFormata = document.querySelector(".sklad--form");
+class App {
+  id = 1;
+  #dataAPP = { warehouses: [], products: [], customers: [], typesOfDrones: [] };
+  #arrayOfOrders = [];
+  vremeZarezdane = 0;
+  #arrayOfSuccesDel = [];
+  #arrayOfRejectedDeliveries = [];
+  totalnotoVreme = 0;
+  constructor() {
+    formata.addEventListener("submit", this.submitForm.bind(this));
+    displayBtn.addEventListener("click", this.displayResults.bind(this));
+    townFormata.addEventListener("submit", this.addTownCord.bind(this));
+    productFormata.addEventListener("submit", this.addProduct.bind(this));
+    custFormata.addEventListener("submit", this.addCustomer.bind(this));
+    skladFormata.addEventListener("submit", this.addWareHouse.bind(this));
   }
-});
-/////////////////////////////
-//Smooth scrolling
-const btnScroll = document.querySelector('.btn--scroll-to');
-btnScroll.addEventListener('click', function () {
-  const pozicijaHero = section1.getBoundingClientRect();
-  /*window.scrollTo({
-    left: pozicijaHero.left + window.pageXOffset,
-    top: pozicijaHero.top + window.pageYOffset,
-    behavior: 'smooth',
-  });*/
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
-///////////////////
-//Page Navigation
-/*
-navLinks.forEach(function (el) {
-  el.addEventListener('click', function (e) {
+  submitForm(e) {
     e.preventDefault();
-    const id = this.getAttribute('href');
-    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-  });
-});
-*/
-navLinks.addEventListener('click', function (e) {
-  e.target.classList.contains('guard') || e.preventDefault();
-  //console.log(e.target);
-  if (
-    e.target.classList.contains('nav__link') &&
-    !e.target.classList.contains('guard')
-  ) {
-    const id = e.target.getAttribute('href');
-    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-  }
-});
-/////////Tabs
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-tabsContainer.addEventListener('click', function (e) {
-  const clicked = e.target.closest('.operations__tab');
-  if (!clicked) return;
-  tabs.forEach(el => el.classList.remove('operations__tab--active'));
-  clicked.classList.add('operations__tab--active');
-  tabsContent.forEach(el => el.classList.remove('operations__content--active'));
-  document
-    .querySelector(`.operations__content--${clicked.dataset.tab}`)
-    .classList.add('operations__content--active');
-});
-////////// Menu fade animation
-const fadeAnimation = function (e) {
-  if (
-    e.target.classList.contains('nav__link') ||
-    e.target.classList.contains('nav__logo')
-  ) {
-    const clicked = e.target;
-    const slika = document.querySelector('.nav__logo');
-    const probaSibling = e.target
-      .closest('.nav')
-      .querySelectorAll('.nav__link');
-    const siblings = [...probaSibling, slika];
-    siblings.forEach(el => {
-      if (el !== clicked) {
-        el.style.opacity = this;
-      }
+    const simulacionoVreme = output.minutes.real / output.minutes.program;
+    const customerID = this.#dataAPP.customers.filter((val) => {
+      return val.name === document.querySelector("#customer").value;
+    })[0].id;
+    const sklad = document.querySelector("#warehouse").value;
+    const odabir = Array.from(
+      document.querySelectorAll(".brojProizvodite")
+    ).map((val) => val.name);
+    const productListata = {};
+    odabir.forEach((val) => {
+      const imetoNaProizvoda = val.split("-")[0];
+      const quantiy = parseInt(
+        document.querySelector(`input[name = "${val}"]`).value
+      );
+      productListata[imetoNaProizvoda] = quantiy;
     });
-  }
-};
-
-const nav = document.querySelector('.nav');
-nav.addEventListener('mouseover', fadeAnimation.bind(0.5));
-nav.addEventListener('mouseout', fadeAnimation.bind(1));
-/////////////Sticky navigation
-const header = document.querySelector('.header');
-const navHeight = nav.getBoundingClientRect().height;
-const navStickyCall = function (entries) {
-  const [entry] = entries;
-  if (entry.isIntersecting) {
-    nav.classList.remove('sticky');
-  } else {
-    nav.classList.add('sticky');
-  }
-};
-const headerObserver = new IntersectionObserver(navStickyCall, {
-  root: null,
-  threshold: 0,
-  rootMargin: `-${navHeight}px`,
-});
-headerObserver.observe(header);
-//////////////Section translation
-const sections = document.querySelectorAll('.section');
-const sectCallBack = function (entries, observer) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove('section--hidden');
-  observer.unobserve(entry.target);
-};
-const sectionObserver = new IntersectionObserver(sectCallBack, {
-  root: null,
-  threshold: 0.15,
-});
-sections.forEach(el => {
-  sectionObserver.observe(el);
-  el.classList.add('section--hidden');
-});
-////////////Images loading
-const images = document.querySelectorAll('.features__img');
-const imgLoader = function (entries, observer) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) return;
-  entry.target.src = entry.target.dataset.src;
-  entry.target.addEventListener('load', function () {
-    entry.target.classList.remove('lazy-img');
-  });
-  observer.unobserve(entry.target);
-};
-const imageObserver = new IntersectionObserver(imgLoader, {
-  root: null,
-  threshold: 0,
-  rootMargin: '200px',
-});
-images.forEach(img => imageObserver.observe(img));
-////////Slider
-const slides = document.querySelectorAll('.slide');
-const rightBtn = document.querySelector('.slider__btn--right');
-const leftBtn = document.querySelector('.slider__btn--left');
-const dotContainer = document.querySelector('.dots');
-///////////////
-const createDots = function () {
-  slides.forEach((s, i) => {
-    dotContainer.insertAdjacentHTML(
-      'beforeend',
-      `<button class="dots__dot" data-slide="${i}"></button>`
+    // Proveri da li dron od sklada e dostatacno zareden ako ne porckata se otkazva
+    const porcka = new Order(customerID, productListata, sklad, this.#dataAPP);
+    //console.log(porcka.warehouse);
+    // Proveri da li dron od sklada e dostatacno zareden ako ne porckata se otkazva
+    const appropriateSklad = this.#dataAPP.warehouses.find(
+      (sklad) => sklad.name === porcka.warehouse
     );
-  });
-};
-createDots();
-const activateDot = function (slide) {
-  document
-    .querySelectorAll('.dots__dot')
-    .forEach(el => el.classList.remove('dots__dot--active'));
-  document
-    .querySelector(`.dots__dot[data-slide="${slide}"]`)
-    .classList.add('dots__dot--active');
-};
-activateDot(0);
-let curSlide = 0;
-const maxSlide = slides.length;
-const goToSlide = function (slide) {
-  slides.forEach((s, i) => {
-    s.style.transform = `translateX(${100 * (i - slide)}%)`;
-  });
-};
-goToSlide(0);
-const nextSlide = function () {
-  if (curSlide === maxSlide - 1) {
-    curSlide = 0;
-  } else {
-    curSlide++;
+    /*--------Za-domasno-2 */
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //this.#arrayOfOrders.push(porcka);
+    if (
+      appropriateSklad.dron.capacity >
+      porcka.time * appropriateSklad.dron.consumption
+    ) {
+      /*---------Namali zaredenosta na drona */
+
+      appropriateSklad.dron.capacity -=
+        porcka.time * appropriateSklad.dron.consumption;
+      porcka.status = "Products delivered";
+      this.#arrayOfSuccesDel.push(porcka);
+      setTimeout(function () {
+        console.log(
+          `Order to customer with id ${porcka.customerId} is delivered in ${porcka.time}`
+        );
+      }, simulacionoVreme);
+
+      ////////////////////
+    } else {
+      //alert(
+      //  "You can not order from selected warehouse, because we do not have available dron, please select another warehouse"
+      //);
+      if (
+        appropriateSklad.dron.capacity <
+        typeOfDrones.find(
+          (item) => item.consumption === appropriateSklad.dron.consumption
+        ).capacity //Ako tozi uslov ne e veren selected skladt e dalec za dostavka do tozi potrebitel
+      ) {
+        console.log("You can not order from this warehouse");
+      } else {
+        appropriateSklad.dron.capacity = typeOfDrones.find(
+          (item) => item.consumption === appropriateSklad.dron.consumption
+        ).capacity;
+        porcka.status = "Rejected";
+        porcka.time = NaN;
+        this.#arrayOfRejectedDeliveries.push(porcka);
+
+        console.log(
+          `Oreder for customer with id ${porcka.customerId} is rejected`
+        );
+      }
+    }
+    divNumberProducts.innerHTML = "";
   }
-  goToSlide(curSlide);
-  activateDot(curSlide);
-};
-const prevSlide = function () {
-  if (curSlide === 0) {
-    curSlide = maxSlide - 1;
-  } else {
-    curSlide--;
+  displayResults() {
+    const time = this.#arrayOfSuccesDel.reduce(
+      (acc, order) => acc + order.time,
+      0
+    );
+    const vremeRecharging = this.#arrayOfRejectedDeliveries.length * 20;
+    const vremetoZaTovarene = (this.#arrayOfSuccesDel.length - 1) * 5; // 5 min za tovarene (-1 poslednata porcka kogato se vrne dronat v sklada)
+    //console.log(vremetoZaTovarene);
+    const totalTime = time + vremetoZaTovarene + vremeRecharging;
+    if (totalTime > 720) {
+      alert(
+        "Number of drones are small to deliver all products in one day. Kmet trebe poveci pari da dade povece skladove ili dronove"
+      );
+    }
+    const textHTML = `<div>Total time for delivery : ${totalTime}</div>`;
+    divRezultatite.insertAdjacentHTML("afterbegin", textHTML);
   }
-  goToSlide(curSlide);
-  activateDot(curSlide);
-};
-rightBtn.addEventListener('click', nextSlide);
-leftBtn.addEventListener('click', prevSlide);
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'ArrowRight') {
-    nextSlide();
+  addTownCord(e) {
+    e.preventDefault();
+    const xTop = +document.querySelector("#top-x").value;
+    const yTop = +document.querySelector("#top-y").value;
+    this.#dataAPP["map-top-right-coordinate"] = { x: xTop, y: yTop };
+    document.querySelector("#top-x").value = "";
+    document.querySelector("#top-y").value = "";
   }
-  if (e.key === 'ArrowLeft') {
-    prevSlide();
+  addProduct(e) {
+    e.preventDefault();
+    const proizvodat = document.querySelector("#ime_proizvoda").value;
+    this.#dataAPP.products.push(proizvodat);
+    const htmlTeksta = `<option value="${proizvodat}">${proizvodat}</option>`;
+    document
+      .querySelector("#products")
+      .insertAdjacentHTML("beforeend", htmlTeksta);
+    document.querySelector("#ime_proizvoda").value = "";
   }
-});
-dotContainer.addEventListener('click', function (e) {
-  if (e.target.classList.contains('dots__dot')) {
-    const slide = e.target.dataset.slide;
-    goToSlide(slide);
-    activateDot(slide);
+  addCustomer(e) {
+    e.preventDefault();
+    const imetoNaCust = document.querySelector("#ime").value;
+    const xKordinatata = +document.querySelector("#x_koordinata").value;
+    const yKordinatata = +document.querySelector("#y_koordinata").value;
+    const novCust = new Customer(
+      this.id,
+      imetoNaCust,
+      xKordinatata,
+      yKordinatata
+    );
+    this.id++;
+    this.#dataAPP.customers.push(novCust);
+    const tekstaZaHtml = `<option value="${imetoNaCust}">${imetoNaCust}</option>`;
+    document
+      .querySelector("#customer")
+      .insertAdjacentHTML("beforeend", tekstaZaHtml);
+    document.querySelector("#ime").value = "";
+    document.querySelector("#x_koordinata").value = "";
+    document.querySelector("#y_koordinata").value = "";
   }
-});
+  addWareHouse(e) {
+    e.preventDefault();
+    const imetoNaSklada = document.querySelector("#imeto_sklad").value;
+    const xSklad = +document.querySelector("#x_sklad").value;
+    const ySklad = +document.querySelector("#y_sklad").value;
+    const skladObekt = new Warehouse(imetoNaSklada, xSklad, ySklad);
+    this.#dataAPP.warehouses.push(skladObekt);
+    const tekstzahtml = `<option value="${skladObekt.name}">${skladObekt.name}</option>`;
+    document
+      .querySelector("#warehouse")
+      .insertAdjacentHTML("beforeend", tekstzahtml);
+    const selectElement = document.getElementById("dron");
+    const selectedOption = selectElement.options[selectElement.selectedIndex];
+    const selectedValue = selectedOption.value;
+    switch (selectedValue) {
+      case "dron1":
+        skladObekt.dron = { capacity: 500, consumption: 1 };
+        break;
+      case "dron2":
+        skladObekt.dron = { capacity: 1000, consumption: 3 };
+        break;
+      case "dron3":
+        skladObekt.dron = { capacity: 2000, consumption: 5 };
+        break;
+    }
+    //console.log(selectedValue);
+    document.querySelector("#imeto_sklad").value = "";
+    document.querySelector("#x_sklad").value = "";
+    document.querySelector("#y_sklad").value = "";
+  }
+}
+class Order {
+  status;
+  constructor(customerId, productList, warehouse, data) {
+    this.data = data;
+    this.customerId = customerId;
+    this.productList = productList;
+    this.warehouse = warehouse;
+    this.distance = this.calculateDistance();
+    this.time = this.calcTimeOneOrder(); //min
+  }
+  calculateDistance() {
+    const { x: x1, y: y1 } = this.data.customers.filter(
+      (cust) => cust.id === this.customerId
+    )[0].coordinates;
+    const { x: x2, y: y2 } = this.data.warehouses.filter(
+      (sklad) => sklad.name === this.warehouse
+    )[0];
+    const rastojanieto = this.calcDistanceFunc(x1, x2, y1, y2);
+    return rastojanieto;
+  }
+  calcDistanceFunc(x1, x2, y1, y2) {
+    /* if (
+      x1 > this.data["map-top-right-coordinate"].x ||
+      x2 > this.data["map-top-right-coordinate"].x ||
+      y1 > this.data["map-top-right-coordinate"].y ||
+      y2 > this.data["map-top-right-coordinate"].y
+    ) {
+      alert("Wrong input");
+      return;
+    }*/
+    const diffInX = x2 - x1;
+    const diffInY = y2 - y1;
+    const distance = +Math.sqrt(diffInX ** 2 + diffInY ** 2);
+    return distance;
+  }
+  calcTimeOneOrder() {
+    const vremetoZaEdinDelivery = +(2 * this.distance).toFixed(2); // Vremeto za edno delivery vremeto na drona od sklada do potrebitela i obratno, po uslovie na zadacata skorosta na drona e ednakva na rastojanieto. Po ispravno moze bi e da se s if provrei koj sklad e s po malko rastojanije od krajnata adresa na potrebitela no za tova njma dostatacno vreme :)
+    return vremetoZaEdinDelivery;
+  }
+}
+class Customer {
+  constructor(id, name, xCord, yCord) {
+    this.id = id;
+    this.name = name;
+    this.coordinates = { x: xCord, y: yCord };
+  }
+}
+class Warehouse {
+  dron;
+  constructor(name, xKor, YKor) {
+    this.name = name;
+    this.x = xKor;
+    this.y = YKor;
+  }
+}
+
+const app = new App();
